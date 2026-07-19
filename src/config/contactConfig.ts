68da6f8be5ef675c0bd53c4ad30125c6f11f5c81
EdemID <84775297+EdemID@users.noreply.github.com>
2026-07-19 20:26:19 +0500

    Open Telegram personal chat

--- src/config/contactConfig.ts
+++ src/config/contactConfig.ts
@@ -1,8 +1,8 @@
 export const contactConfig = {
   phone: '+7 (977) 634-19-20',
   phoneHref: 'tel:+79776341920',
-  telegramLabel: '@rentcar_krasnodar',
-  telegramUrl: 'https://t.me/rentcar_krasnodar',
+  telegramLabel: 'Telegram',
+  telegramUrl: 'https://t.me/+79776341920',
   whatsappUrl: 'https://wa.me/79776341920',
   whatsappPhone: '79776341920',
   smsPhone: '+79776341920',
