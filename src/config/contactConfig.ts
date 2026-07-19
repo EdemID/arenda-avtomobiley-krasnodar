a7c43084bbd278184dbebbe1705b6f2a0b666541
EdemID <84775297+EdemID@users.noreply.github.com>
2026-07-19 20:09:33 +0500

    Update contact channels

--- src/config/contactConfig.ts
+++ src/config/contactConfig.ts
@@ -1,6 +1,10 @@
 export const contactConfig = {
-  phone: '',
-  telegramUrl: '',
-  whatsappUrl: '',
+  phone: '+7 (977) 634-19-20',
+  phoneHref: 'tel:+79776341920',
+  telegramLabel: '@rentcar_krasnodar',
+  telegramUrl: 'https://t.me/rentcar_krasnodar',
+  whatsappUrl: 'https://wa.me/79776341920',
+  whatsappPhone: '79776341920',
+  smsPhone: '+79776341920',
   email: '',
 }
